c1bb35d80cec5f355e896cb1e7cd6bb029e23a19
Pujan Modha <pujanmodha19@gmail.com>
2023-10-01 03:55:06 +0530

    Minor Fixes

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -18,11 +18,11 @@ function Resume() {
                           <div>
                               <h2 className='text-lg font-semibold'>CONTACT</h2>
                               <ul>
-                                  <li><span className='font-semibold text-primary'>GitHub: </span><span className='hover:text-[#44475a] text-[#6272a4] font-light'>github.com/pujan-modha<span className='text-primary opacity-50'>↗</span></span></li>
-                                  <li><span className='font-semibold text-primary'>LinkedIn: </span><span className='hover:text-[#44475a] text-[#6272a4] font-light'>linkedin.com/in/pujan-modha<span className='text-primary opacity-50'>↗</span></span></li>
-                                  <li><span className='font-semibold text-primary'>Twitter: </span><span className='hover:text-[#44475a] text-[#6272a4] font-light'>twitter.com/pujan_modha<span className='text-primary opacity-50'>↗</span></span></li>
-                                  <li><span className='font-semibold text-primary'>E-Mail: </span><span className='hover:text-[#44475a] text-[#6272a4] font-light'>pujan@ieee.org<span className='text-primary opacity-50'>↗</span></span></li>
-                                  <li><span className='font-semibold text-primary'>Phone: </span><span className='hover:text-[#44475a] text-[#6272a4] font-light'>+91 9484856107<span className='text-primary opacity-50'>↗</span></span></li>
+                                  <li><span className='font-semibold text-primary'>GitHub: </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>github.com/pujan-modha<span className='text-primary opacity-50'>↗</span></span></a></li>
+                                  <li><span className='font-semibold text-primary'>LinkedIn: </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>linkedin.com/in/pujan-modha<span className='text-primary opacity-50'>↗</span></span></a></li>
+                                  <li><span className='font-semibold text-primary'>Twitter: </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>twitter.com/pujan_modha<span className='text-primary opacity-50'>↗</span></span></a></li>
+                                  <li><span className='font-semibold text-primary'>E-Mail: </span><a href="mailto:pujan@ieee.org" target="_noref"><span className='hover:text-primary text-secondary font-light'>pujan@ieee.org<span className='text-primary opacity-50'>↗</span></span></a></li>
+                                  <li><span className='font-semibold text-primary'>Phone: </span><a href="tel:+91 9484856107" target="_noref"><span className='hover:text-primary text-secondary font-light'>+91 9484856107<span className='text-primary opacity-50'>↗</span></span></a></li>
                               </ul>
                           </div>
                           <hr className='md:hidden mx-auto border border-opacity-50 border-foreground my-[2.5%] w-full'/>
@@ -48,7 +48,7 @@ function Resume() {
                       <div className='grid md:grid-cols-2'>
                           <div className='md:row-span-2 gap-y-[5%]'>
                               <div>
-                                  <h2 className='font-semibold text-primary'>LANGUAGES</h2>
+                                  <p className='font-semibold text-primary'>LANGUAGES</p>
                                   <div>
                                       <ul className='flex flex-wrap gap-[1%]'>
                                           <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>C/C++</li>
@@ -63,7 +63,7 @@ function Resume() {
                                   </div>
                               </div>
                               <div>
-                                  <h2 className='font-semibold text-primary'>TECHNOLOGIES</h2>
+                                  <p className='font-semibold text-primary'>TECHNOLOGIES</p>
                                   <div>
                                       <ul className='flex flex-wrap gap-[1%]'>
                                           <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>React.js</li>
@@ -82,13 +82,14 @@ function Resume() {
                               </div>
                           </div>
                           <div>
-                              <h2 className='font-semibold text-primary'>SOFT SKILLS</h2>
+                              <p className='font-semibold text-primary'>SOFT SKILLS</p>
                               <div>
                                   <ul className='flex flex-wrap gap-[1%]'>
                                       <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Communication</li>
                                       <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Logical Thinking</li>
                                       <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Problem Solving</li>
                                       <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Management</li>
+                                      <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Marketing</li>
                                       <li className='text-sm bg-secondary text-background px-[2.5%] mb-[1%]'>Networking</li>
                                   </ul>
                               </div>
@@ -153,7 +154,7 @@ function Resume() {
                       <div className='mb-[2.5%]'>
                           <div className='grid md:grid-cols-2'>
                               <div>
-                                  <p className='font-semibold text-primary'>IEEE STUDENT BRANCH | WEBMASTER</p>
+                                  <p className='font-semibold text-primary hover:text-secondary'><a href="/" target="_noref">PROJECT - 1 | GITHUB<span className='text-primary opacity-50 font-thin text-sm'>↗</span></a></p>
                               </div>
                               <div>
                                   <p className='text-secondary text-xs font-medium md:text-right'>(May 2023 - Present)</p>
@@ -169,7 +170,7 @@ function Resume() {
                       <div className='mb-[2.5%]'>
                           <div className='grid md:grid-cols-2'>
                               <div>
-                                  <p className='font-semibold text-primary'>IEEE STUDENT BRANCH | WEBMASTER</p>
+                                  <p className='font-semibold text-primary hover:text-secondary'><a href="/" target="_noref">PROJECT - 2 | GITHUB<span className='text-primary opacity-50 font-thin text-sm'>↗</span></a></p>
                               </div>
                               <div>
                                   <p className='text-secondary text-xs font-medium md:text-right'>(May 2023 - Present)</p>
@@ -190,9 +191,9 @@ function Resume() {
                               <h2 className='text-lg font-semibold'>CERTIFICATIONS</h2>
                               <div className='text-sm'>
                                   <ul>
-                                      <li><span className='font-black text-secondary'> › </span><span className='hover:text-secondary text-primary font-light'>Course 1 - Coursera</span><span> </span><span className='hover:text-primary group-hover:opacity-100 text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
-                                      <li><span className='font-black text-secondary'> › </span><span className='hover:text-secondary text-primary font-light'>Course 2 - Udemy</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
-                                      <li><span className='font-black text-secondary'> › </span><span className='hover:text-secondary text-primary font-light'>Course 3 - Udacity</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='hover:text-secondary text-primary font-light'>Course 1 - Coursera</span></a><span> </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='hover:text-secondary text-primary font-light'>Course 2 - Udemy</span></a><span> </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='hover:text-secondary text-primary font-light'>Course 3 - Udacity</span></a><span> </span><a href="/" target="_noref"><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
                                   </ul>
                               </div>
                           </div>
@@ -201,9 +202,9 @@ function Resume() {
                               <h2 className='text-lg font-semibold'>ACHIEVEMENTS</h2>
                               <div className='text-sm'>
                                   <ul>
-                                      <li><span className='font-black text-secondary'> › </span><span className='text-primary font-light'>Achievement - 1</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
-                                      <li><span className='font-black text-secondary'> › </span><span className='text-primary font-light'>Achievement - 2</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
-                                      <li><span className='font-black text-secondary'> › </span><span className='text-primary font-light'>Achievement - 3</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='text-primary font-light'>Achievement - 1</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='text-primary font-light'>Achievement - 2</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
+                                      <li><span className='font-black text-secondary'> › </span><a href="/" target="_noref"><span className='text-primary font-light'>Achievement - 3</span><span> </span><span className='hover:text-primary text-secondary font-light'>(Certificate<span className='text-primary opacity-50'>↗</span>)</span></a></li>
                                   </ul>
                               </div>
                           </div>
--- tailwind.cjs
+++ tailwind.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [],
   presets: [],
   darkMode: 'media', // or 'class'
