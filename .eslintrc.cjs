26cd486b1b68d2ba95fd139cb20a7a85e4b079a1
Pujan Modha <pujanmodha19@gmail.com>
2023-10-03 02:12:55 +0530

    minor lint changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,30 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
